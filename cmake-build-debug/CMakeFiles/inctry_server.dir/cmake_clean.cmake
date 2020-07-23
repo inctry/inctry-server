367bf05f572dbc69052250d4a1862806baebe077
Inctry <inctry@outlook.com>
2020-07-23 16:58:21 +0800

    CMake initial

---
+++ cmake-build-debug/CMakeFiles/inctry_server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inctry_server.dir/main.cpp.obj"
+  "inctry_server.exe"
+  "inctry_server.exe.manifest"
+  "inctry_server.pdb"
+  "libinctry_server.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inctry_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
